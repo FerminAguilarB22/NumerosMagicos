314bcae13813d88e87ced11654c4e4e89b214dbd
Fermin Aguilar <ferminab99@gmail.com>
2021-09-28 16:49:35 -0300

    ejercicio corregido

--- js/app.js
+++ js/app.js
@@ -1,24 +1,18 @@
-function numeroAleatorio(){
-    const min = 0;
-    const max = 100;
-    function numero (min, max) {
-        return Math.floor (Math.random() * (max - min) + min);
-      }
-      console.log(numero(min,max));
-     
+let numero;
+
+function numeroAleatorio() {
+  numero = Math.floor(Math.random() * 11);
+  console.log(numero);
 }
-function adivinarNumero(){
-    var botonAdivinar =parseInt( document.getElementById("numero").value);
-    console.log(botonAdivinar);
+function adivinarNumero() {
+  var botonAdivinar = parseInt(document.getElementById("numero").value);
+  console.log(botonAdivinar);
+
+  if (numero == botonAdivinar){
+      alert("adivinaste")
+  }else {
+      alert("vuelva a intenter")
+  }
 }
 
 
-// if (numeroAleatorio == adivinarNumero){
-//     console.log("son iguales");
-//     // let section = document.getElementsByTagName("section");
-//     // section[1].innerHTML = `<h1 class="display-4 mt-5">LOS NUMEROS SON IGUALES</h1>`
-// }else if(numeroAleatorio>adivinarNumero){
-//     alert("El numero ingresado es menor al numero magico");
-// }else{
-//     alert("El numero ingresado es mayor al numero magico");
-// }
